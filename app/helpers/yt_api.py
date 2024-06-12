3f5267273793dbb7b8678147bdf74271ed12ebe7
ibrahiM10 <ibrahim.sma1206@gmail.com>
2024-06-12 11:46:57 -0400

    app Agregada.

---
+++ app/helpers/yt_api.py
@@ -0,0 +1,34 @@
+import googleapiclient.discovery
+import googleapiclient.errors
+import json
+
+API_KEY = 'AIzaSyC-kam92VgyxI26NfhJyLM3PeDSLS_ImZQ'
+
+def generate_video_url(video_id: str, channel_name: str):
+    yt_video_url = f"https://www.youtube.com/watch?v={video_id}&ab_channel={channel_name}"
+    return yt_video_url
+
+def main():
+    api_service_name = "youtube"
+    api_version = "v3"
+
+    # Get credentials and create an API client
+    youtube = googleapiclient.discovery.build(
+        api_service_name, api_version, developerKey=API_KEY)
+
+    request = youtube.search().list(
+        part="snippet",
+        q="reutilizar agua al lavar ropa",
+        maxResults=3
+    )
+    response = request.execute()
+    # data = json.dumps(response)
+    
+    for i in range(len(response["items"])):
+        video_id = response["items"][i]["id"]["videoId"]
+        channel_name = response["items"][i]["snippet"]["channelTitle"]
+        vid_url = generate_video_url(video_id, channel_name)
+        print(f"Here's the url of the video {vid_url}")
+
+
+main()
---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Vista de la pagina de inicio.
+
+# Vista del formulario para agregar una actividad.
+
+# Vista de una lista de todas las actividades ingresadas por el usuario.
+
+# Vista de la actividad seleccionada junto a su video con la metodologia recomendada.
